23bbcebaba126dad2c5af1b5f8be3434ff34ffb9
Paul Sandford <paul.sandford@bjss.com>
2024-09-11 10:58:51 +0100

    refactor getUsers() to make use of GetUsers() from inmemory.go

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+    "acme/db"
+    "encoding/json"
 	"fmt"
 	"net/http"
+	"io"
 )
 
 func main() {
 
 	http.HandleFunc("/", rootHandler)
+    http.HandleFunc("/api/users", getUsers)
 
 	// Starting the HTTP server on port 8080
 	fmt.Println("Server listening on port 8080...")
@@ -18,6 +22,25 @@ func main() {
 }
 
 func rootHandler(writer http.ResponseWriter, request *http.Request) {
-    fmt.Fprintf(writer, "Hello, World!")
+    io.WriteString(writer, "Hello, World!")
 }
 
+func getUsers(writer http.ResponseWriter, request *http.Request) {
+    fmt.Printf("got /api/users request\n")
+    
+    users := db.GetUsers()
+
+    usersJSON, errMarshal := json.Marshal(users)
+    if errMarshal != nil {
+        http.Error(writer, "Internal Server Error", http.StatusInternalServerError)
+        return
+    }
+
+    writer.Header().Set("Content-Type", "application/json")
+    
+    _, err := writer.Write(usersJSON)
+    if err != nil {
+        http.Error(writer, "Internal Server Error", http.StatusInternalServerError)
+        return
+    }
+}
