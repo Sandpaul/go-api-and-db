f678c72b8ed7c25a303f62a70c31345109565b64
Paul Sandford <paul.sandford@bjss.com>
2024-09-11 14:26:11 +0100

    add test to confirm DeleteUser() returns false when passed invalid id.

--- db/inmemory_test.go
+++ db/inmemory_test.go
@@ -6,6 +6,7 @@ import (
 )
 
 func TestDeleteUser(t *testing.T) {
+	ResetUsers()
 
 	result := DeleteUser(2)
 
@@ -20,5 +21,25 @@ func TestDeleteUser(t *testing.T) {
 
 	if result != true {
 		t.Errorf("Expected true, got %v", result)
+	}
+}
+
+func TestDeleteUserInvalidId(t *testing.T) {
+	ResetUsers()
+
+	result := DeleteUser(7)
+
+	expectedUsers := []User{
+		{ID: 1, Name: "User 1"},
+		{ID: 2, Name: "User 2"},
+		{ID: 3, Name: "User 3"},
+	}
+
+	if !slices.Equal(users, expectedUsers) {
+		t.Errorf("Expected users %v, but got %v", expectedUsers, users)
+	}
+
+	if result != false {
+		t.Errorf("Expected false, got %v", result)
 	}
 }
