11783f290492afadd0c8fdb356601b426f61979b
Paul Sandford <paul.sandford@bjss.com>
2024-09-11 12:24:47 +0100

    refactor main.go to use router, extracted handleUsers() into getUsers() and createUser().

--- main.go
+++ main.go
@@ -10,12 +10,15 @@ import (
 
 func main() {
 
-	http.HandleFunc("/", rootHandler)
-	http.HandleFunc("/api/users", handleUsers)
+    router := http.NewServeMux()
 
-	// Starting the HTTP server on port 8080
+    router.HandleFunc("GET /", rootHandler)
+    router.HandleFunc("GET /api/users", getUsers)
+    router.HandleFunc("POST /api/users", createUser)
+
+	// Starting the HTTP server on port 8080 and providing router variable to ListenAndServe
 	fmt.Println("Server listening on port 8080...")
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", router)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
@@ -25,25 +28,7 @@ func rootHandler(writer http.ResponseWriter, request *http.Request) {
 	io.WriteString(writer, "Hello, World!")
 }
 
-func handleUsers(writer http.ResponseWriter, request *http.Request) {
-	
-    if request.Method == http.MethodPost {
-
-        var user db.User
-        err := json.NewDecoder(request.Body).Decode(&user)
-        if err != nil {
-            fmt.Println("Error decoding request body:", err)
-            http.Error(writer, "Bad Request", http.StatusBadRequest)
-            return
-        }
-
-        id := db.AddUser(user)
-        writer.WriteHeader(http.StatusCreated)
-        fmt.Fprintf(writer, "User created successfully: %d", id)
-
-        return
-    }
-    
+func getUsers(writer http.ResponseWriter, request *http.Request) {
     fmt.Printf("got /api/users request\n")
 
 	users := db.GetUsers()
@@ -61,4 +46,19 @@ func handleUsers(writer http.ResponseWriter, request *http.Request) {
 		http.Error(writer, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
+}
+
+func createUser(writer http.ResponseWriter, request *http.Request) {
+
+        var user db.User
+        err := json.NewDecoder(request.Body).Decode(&user)
+        if err != nil {
+            fmt.Println("Error decoding request body:", err)
+            http.Error(writer, "Bad Request", http.StatusBadRequest)
+            return
+        }
+
+        id := db.AddUser(user)
+        writer.WriteHeader(http.StatusCreated)
+        fmt.Fprintf(writer, "User created successfully: %d", id)
 }
