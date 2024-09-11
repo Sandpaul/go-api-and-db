4b2b7edb3f7f712f1dd000de642b9061a33edc4d
Paul Sandford <paul.sandford@bjss.com>
2024-09-11 10:58:04 +0100

    create db/inmemory.go to initialise user test data.

---
+++ db/inmemory.go
@@ -0,0 +1,20 @@
+package db
+
+type User struct {
+    ID int `json:"id`
+    Name string `json:"name"`
+}
+
+var users []User
+
+func init() {
+	users = []User{
+        {ID: 1, Name: "User 1"},
+        {ID: 2, Name: "User 2"},
+		{ID: 3, Name: "User 3"},
+    }
+}
+
+func GetUsers() []User {
+	return users
+}
