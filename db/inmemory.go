95264491c7b2d54c107a20a6100cbb2afa9b39f7
Paul Sandford <paul.sandford@bjss.com>
2024-09-11 12:15:00 +0100

    create AddUser().

--- db/inmemory.go
+++ db/inmemory.go
@@ -6,6 +6,7 @@ type User struct {
 }
 
 var users []User
+var count int = 3
 
 func init() {
 	users = []User{
@@ -17,4 +18,13 @@ func init() {
 
 func GetUsers() []User {
 	return users
+}
+
+func AddUser(user User) (id int) {
+	count++
+	user.ID = count
+
+	users = append(users, user)
+
+	return count
 }
