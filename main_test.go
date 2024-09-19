abda105c349cdaf39588c24ba12705dd6b3870c2
Paul Sandford <paul.sandford@bjss.com>
2024-09-19 14:37:12 +0100

    create mock and use in main.go test.

--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"acme/api"
+	"acme/db/mock"
 	"acme/model"
 	"acme/repository/user"
 	"acme/service"
@@ -243,4 +244,37 @@ func TestDeleteUserHandlerNotFound(t *testing.T) {
 	if status := rr.Code; status != http.StatusNotFound {
 		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusNotFound)
 	}
+}
+
+func TestGetUsersHandlerWithMock(t *testing.T) {
+    req, err := http.NewRequest("GET", "/api/users", nil)
+    if err != nil {
+        t.Fatal(err)
+    }
+    rr := httptest.NewRecorder()
+    expected := []model.User{
+        {ID: 1, Name: "Alice"},
+        {ID: 2, Name: "Bob"},
+        {ID: 3, Name: "Terry"},
+    }
+    mockRepo := &mock.MockRepository{
+		MockGetUsers: func() ([]model.User, error) {
+			return expected, nil
+		},
+	}
+    userService := service.NewUserService(mockRepo)
+    userAPI := api.NewUserAPI(userService)
+    
+    handler := http.HandlerFunc(userAPI.GetUsers)
+    handler.ServeHTTP(rr, req)
+    if status := rr.Code; status != http.StatusOK {
+        t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+    }
+	var actual []model.User
+    if err := json.Unmarshal(rr.Body.Bytes(), &actual); err != nil {
+        t.Fatalf("Failed to unmarshal response body: %v", err)
+    }
+    if !reflect.DeepEqual(actual, expected) {
+        t.Errorf("handler returned unexpected body: got %v want %v", actual, expected)
+    }
 }
